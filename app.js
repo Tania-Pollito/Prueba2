611d9815b84683f189182c4dbf78026bfa87f566
Tania-Pollito <taniamorenomorales@gmail.com>
2025-06-23 16:11:32 -0500

    Añadiendo estilos css

---
+++ app.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const formulario = document.getElementById('formulario-tarea');
+    const listaTareas = document.getElementById('lista-tareas');
